d76e677007233687f130366fa82b5ef012b7ed4c
Name <email@exaple.ru>
2023-06-05 08:04:05 +0300

    Добавьте файлы проекта.

---
+++ OOPModule1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPModule1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pen pen = new Pen();
+            pen.Show();
+        }
+        class Pen
+        {
+            public string color;
+            public int cost;
+            public Pen()
+            {
+                color = "черный";
+                cost = 100;
+            }
+            public Pen(string penColor, int penCost)
+            {
+                color = penColor;
+                cost = penCost;
+            }
+            public void Show() 
+            {
+                Console.WriteLine("Цвет : {0}, Значение : {1}",color,cost);
+            }
+        }
+    }
+}
